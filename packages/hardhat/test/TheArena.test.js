2b51e485ce3392b58e0beb653c4b709948f8d445
Sparadrap1101 <alexis111@orange.fr>
2023-12-21 01:34:02 +0100

    Add 'MintFighter' unit tests TODO for 'TheArena'

--- packages/hardhat/test/TheArena.test.js
+++ packages/hardhat/test/TheArena.test.js
@@ -3,3 +3,14 @@
 // Constructor :
 // // - Verify if 'weaponsScoreUnit' is set
 // // - Verify if Chainlink VRF works well
+
+// MintFighter :
+// // - Verify it creates the NFT with msg.value of '0.001 eth'
+// // - Verify it returns the 'tokenId'
+// // - Verify the tokenId has been incremented
+// // - Verify it has the good name
+// // - Verify the new Fighter is stored in 'fighters[]' mapping
+// // - Verify the new Request is stored in 'randomRequests[]' mapping
+// // - Verify the new Fighter is owned by msg.sender
+// // - Verify the NFT balance of the owner has been incremented
+// // - Verify the event 'MintFighter()' is emit
