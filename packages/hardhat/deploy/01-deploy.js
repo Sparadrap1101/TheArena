3ebe4bcce23489dbccffbf585cceb99b50f000b3
Sparadrap1101 <alexis111@orange.fr>
2023-12-24 01:21:52 +0100

    Add 'deployer' to '01-deploy.js'

--- packages/hardhat/deploy/01-deploy.js
+++ packages/hardhat/deploy/01-deploy.js
@@ -1,6 +1,9 @@
 const { network, ethers } = require("hardhat");
 const { verify } = require("../utils/verify.js");
 
-module.exports = async function ({ getNamedAccounts, deployments }) {};
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+};
 
 module.exports.tags = ["all", "theArena", "deployments"];
