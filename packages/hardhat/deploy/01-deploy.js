166a083b45c3f360156ef65040a0203e39d96bd7
Sparadrap1101 <alexis111@orange.fr>
2023-12-24 01:20:47 +0100

    Add 'module.exports' to deployment script

--- packages/hardhat/deploy/01-deploy.js
+++ packages/hardhat/deploy/01-deploy.js
@@ -1,2 +1,6 @@
 const { network, ethers } = require("hardhat");
 const { verify } = require("../utils/verify.js");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {};
+
+module.exports.tags = ["all", "theArena", "deployments"];
