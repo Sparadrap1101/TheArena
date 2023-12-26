9c003cab899459fdf40740a9e5fec788b4cf4532
Sparadrap1101 <alexis111@orange.fr>
2023-12-26 04:25:00 +0100

    Add 'vrfCoordinatorAddr' in '01-deploy.js'

--- packages/hardhat/deploy/01-deploy.js
+++ packages/hardhat/deploy/01-deploy.js
@@ -4,6 +4,8 @@ const { verify } = require("../utils/verify.js");
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  const vrfCoordinatorAddr = "";
 };
 
 module.exports.tags = ["all", "theArena", "deployments"];
