c2ee22a6259e7f3575d677b8063bac701a56c0b8
Sparadrap1101 <alexis111@orange.fr>
2024-01-02 22:31:45 +0100

    Add scores in 'weaponsScoreUnit'

--- packages/hardhat/deploy/01-deploy.js
+++ packages/hardhat/deploy/01-deploy.js
@@ -8,7 +8,10 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   const vrfCoordinatorAddr = "";
   const keyHash = "";
   const subscriptionId = 0;
-  const weaponsScoreUnit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const weaponsScoreUnit = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+    31, 32, 33,
+  ];
 
   const args = [vrfCoordinatorAddr, keyHash, subscriptionId, weaponsScoreUnit];
 
